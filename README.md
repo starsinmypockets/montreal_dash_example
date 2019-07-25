945b4bc1cb9bb0b1f09e4b68503669131f56b856
starsinmypockets <starsinmypockets@gmail.com>
2019-07-25 16:44:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Example Dashboard
+
+### Configuring CKAN NG
+
+### script.js
+
+
+### dashboard-js
+
+### Vega
+
---
+++ dashboard.js
@@ -0,0 +1,33 @@
+var config = {
+  widgets: [
+    {
+      "elementId": "testWidget",
+      "view": {
+        "title": "Economy",
+        "legend": "Recent unemployment rate",
+        "resources": [
+          {
+            "datasetId": "unemployment",
+            "name": "unemployment-rate",
+            "transform": [
+              {
+                "type": "aggregate",
+                "fields": [
+                  "date"
+                ],
+                "operations": [
+                  "argmax"
+                ]
+              }
+            ]
+          }
+        ],
+        "specType": "figure",
+        "spec": {
+          "fieldName": "unemployment_rate"
+        }
+      }
+    }
+  ],
+  datasets: ["https://datahub.io/london/unemployment"]
+}
